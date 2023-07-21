91f9f6b870eebf36ddf3321cdf2969c924f7cfab
coders-rabbi <pcrabbi2020@gmail.com>
2023-07-21 20:45:43 +0600

    no change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express"); 
-const app=express(); 
-const port=process.env.PORT || 5000;
-const cors = require("cors"); 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
 require("dotenv").config();
 const jwt = require('jsonwebtoken');
 
@@ -20,7 +20,7 @@ if (!fs.existsSync(uploadDir)) {
 
 // Midelware  
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
@@ -38,11 +38,11 @@ const verifyJWT = (req, res, next) => {
     next();
   })
 }
- 
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ts9e2p2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -58,111 +58,137 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-    const usersCollection=client.db("flynass").collection("users")
-    const orderCollection=client.db("flynass").collection("orders")
+
+    const usersCollection = client.db("flynass").collection("users")
+    const orderCollection = client.db("flynass").collection("orders")
 
     // jwt token create 
-    app.post("/jwt",(req,res)=>{
-      const user=req.body;
-      const token=jwt.sign(user,process.env.Access_token,{expiresIn:"1h"}) 
-      res.send({token})
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.Access_token, { expiresIn: "1h" })
+      res.send({ token })
     })
 
-    app.post("/users",async(req,res)=>{
-    users=req.body; 
+    app.post("/users", async (req, res) => {
+      users = req.body;
       console.log(users);
-      const query={email:users.email}; 
-      const existingusers=await usersCollection.findOne(query)
-      if(existingusers)
-      {
-        return res.send({message:'Merchant already exists'})
+      const query = { email: users.email };
+      const existingusers = await usersCollection.findOne(query)
+      if (existingusers) {
+        return res.send({ message: 'Merchant already exists' })
       }
-      const result=await merchantCollection.insertOne(users);
+      const result = await merchantCollection.insertOne(users);
       res.send(result)
     })
 
-    app.get("/user/:email",async(req,res)=>{
-      const email=req.params.email;
-      const quary={email:email};
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const quary = { email: email };
 
-      const result=await usersCollection.findOne(quary);
+      const result = await usersCollection.findOne(quary);
       res.send(result)
     })
 
-    app.post("/orders",async(req,res)=>{
-     const order=req.body;
-     const result=await orderCollection.insertOne(order);
-     res.send(result)
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result)
     })
 
-    app.get("/orders",async(req,res)=>{
-      const result=await orderCollection.find().toArray();
+    app.get("/orders", async (req, res) => {
+      const result = await orderCollection.find().toArray();
       res.send(result)
-    }) 
+    })
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.patch('/user/status/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // for csv file upload  
-         // File upload configuration
-  const upload = multer({ dest: "uploads/" });
+    // File upload configuration
+    const upload = multer({ dest: "uploads/" });
 
-  // File upload endpoint
-  app.post("/upload", upload.single("csvFile"), (req, res) => {
-    const file = req.file;
-    console.log(file);
-    const user_email = req.body.user_email;
-    if (!file) {
-      return res.status(400).json({ error: "No CSV file uploaded" });
-    }
+    // File upload endpoint
+    app.post("/upload", upload.single("csvFile"), (req, res) => {
+      const file = req.file;
+      console.log(file);
+      const user_email = req.body.user_email;
+      if (!file) {
+        return res.status(400).json({ error: "No CSV file uploaded" });
+      }
 
-    const results = [];
+      const results = [];
 
-    // Parse CSV file and save data to MongoDB
-    fs.createReadStream(file.path)
-      .pipe(csvParser())
-      .on("data", (row) => {
-        // Assuming the CSV has columns 'name', 'age', 'email'
-        // Adjust this according to your actual CSV structure
-        results.push({
-        referenceNumber: row.ID,
-        name: row.NAME,
-        user_email: user_email,
+      // Parse CSV file and save data to MongoDB
+      fs.createReadStream(file.path)
+        .pipe(csvParser())
+        .on("data", (row) => {
+          // Assuming the CSV has columns 'name', 'age', 'email'
+          // Adjust this according to your actual CSV structure
+          results.push({
+            referenceNumber: row.ID,
+            name: row.NAME,
+            user_email: user_email,
+          });
+        })
+        .on("end", () => {
+          // Save the data to MongoDB
+          orderCollection.insertMany(results, (err, result) => {
+            if (err) {
+              res.status(500).json({ error: "Error saving data to MongoDB" });
+            } else {
+              const result = res.status(200).json({ message: "Data saved to MongoDB successfully" });
+              console.log(result);
+            }
+          });
         });
-      })
-      .on("end", () => {
-        // Save the data to MongoDB
-        orderCollection.insertMany(results, (err, result) => {
-          if (err) {
-            res.status(500).json({ error: "Error saving data to MongoDB" });
-          } else {
-           const result= res.status(200).json({ message: "Data saved to MongoDB successfully" });
-            console.log(result);
-          }
-        });
-      });
-  });
+    });
 
 
-  // admin and superAdmin finding ============================
-  app.get("/user/admin/:email",async(req,res)=>{
-    const email=req.params.email;
-    const quary={email:email};
+    // admin and superAdmin finding ============================
+    app.get("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const quary = { email: email };
 
-    const user=await usersCollection.findOne(quary);
-    const result={admin:user?.role==="admin"}
-    res.send(result);
-  });
+      const user = await usersCollection.findOne(quary);
+      const result = { admin: user?.role === "admin" }
+      res.send(result);
+    });
 
 
-  app.get("/user/superadmin/:email",async(req,res)=>{
-    const email=req.params.email;
-    const quary={email:email};
+    app.get("/user/superadmin/:email", async (req, res) => {
+      const email = req.params.email;
+      const quary = { email: email };
 
-    const user=await usersCollection.findOne(quary);
-    const result={super_admin:user?.role==="superAdmin"}
-    res.send(result);
-  })
-// ===============================================================
+      const user = await usersCollection.findOne(quary);
+      const result = { super_admin: user?.role === "superAdmin" }
+      res.send(result);
+    })
+    // ===============================================================
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -174,10 +200,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get("/",(req,res)=>{
-    res.send("Flynass Server is Running.........")
+app.get("/", (req, res) => {
+  res.send("Flynass Server is Running.........")
 });
 
-app.listen(port,()=>{
-    console.log(`Flynass server is running on ${port}`);
+app.listen(port, () => {
+  console.log(`Flynass server is running on ${port}`);
 });
