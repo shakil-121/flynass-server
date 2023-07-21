ff9fe34dd2c3547b9369977ec2e4190af94942a8
coders-rabbi <pcrabbi2020@gmail.com>
2023-07-21 16:23:07 +0600

    no changes

--- index.js
+++ index.js
@@ -78,19 +78,7 @@ async function run() {
       {
         return res.send({message:'Merchant already exists'})
       }
-      const result=await usersCollection.insertOne(users);
-      res.send(result)
-    }) 
-
-    app.get("/user/:email",async(req,res)=>{
-      const email=req.params.email;
-
-      const result=await usersCollection.findOne({email:email});
-      res.send(result)
-    }) 
-
-    app.get("/users",async(req,res)=>{
-      const result =await usersCollection.find().toArray()
+      const result=await merchantCollection.insertOne(users);
       res.send(result)
     })
 
@@ -163,7 +151,7 @@ async function run() {
     const user=await usersCollection.findOne(quary);
     const result={admin:user?.role==="admin"}
     res.send(result);
-  })
+  });
 
 
   app.get("/user/superadmin/:email",async(req,res)=>{
@@ -173,8 +161,23 @@ async function run() {
     const user=await usersCollection.findOne(quary);
     const result={super_admin:user?.role==="superAdmin"}
     res.send(result);
-  })
+  });
 // ===============================================================
+// filter by date 
+app.get("/orders/today", async (req, res) => {
+  const today = new Date();
+  const formattedToday = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+console.log(formattedToday);
+  const query = { date: formattedToday }; // Assuming the date field is named 'date'
+  
+  try {
+    const result = await orderCollection.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching orders for today:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
